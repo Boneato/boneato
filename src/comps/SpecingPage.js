2f7bbe21387ee8527def71e75e6bf80456d914d0
Uyedacb <uyedacb@uw.edu>
2019-10-29 01:20:22 -0700

    finish SpecingPage

--- src/comps/SpecingPage.js
+++ src/comps/SpecingPage.js
@@ -1,3 +1,30 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import 
+import LocationList from './modules/LocationsList';
+import LocationsController from '../cont/LocationsController';
+import firebase from 'firebase';
+require('firebase/firestore');
+
+export class SpecingPage extends Component {
+    // takes in ingredientID
+    constructor(props) {
+
+    }
+
+    // take in valid ingredientID, query firebase for IngredientModel
+    // run LocationsController to capture list of relevant LocationModel(s)
+    // store dictionary of LocationModel(s) and IngredientModel into state
+    oneItemController = (ingredientID) => {
+
+    }
+
+    // run oneItemController
+    componentDidMount() {
+
+    }
+
+    // render LocationList with LocationModel(s) and IngredientModel
+    render() {
+
+    }
+}
--- src/comps/modules/LocationsList.js
+++ src/comps/modules/LocationsList.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import VotingController from '../../cont/VotingController';
+import LocationInfo from '../location/LocationInfo';
+import UpvoteButton from '../location/UpVoteButton';
+import DownvoteButton from '../location/DownVoteButton';
 
 export class LocationsList extends Component {
     
     // pre-conditions: 
-    //      props must have list of LocationModels and respective LocationItems
+    //      props must have list of LocationModels with respective LocationItems
     constructor(props) {
 
     }
