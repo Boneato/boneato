0ae74396d05a2ff6ed3b543ec2ca57f4dd6e0dce
xCyziw <neonlight003@gmail.com>
2019-10-29 14:21:57 -0700

    create stubs for TeamCard and AboutPage

---
+++ src/comps/AboutPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+export class AboutPage extends Component {
+    constructor(props) {
+        
+    }
+    
+    // renders a TeamCard for each team member, as well as a paragraph description of the project
+    render() {
+
+    }
+}
---
+++ src/comps/modules/TeamCard.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Modal from '../modules/Modal';
+import MapsController from '../../cont/MapsController';
+
+export class TeamCard extends Component {
+    
+    constructor(props) {
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    // renders a team member image, team member first name, and team member role
+    render() {
+
+    }
+}
