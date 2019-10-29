45ea542ceb539a9388b8c21af3e99922249e4813
Brian Uyeda <uyedacb@uw.edu>
2019-10-29 14:17:10 -0700

    finish location comps

---
+++ src/comps/location/DownVoteButton.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+export class DownVoteButton extends Component {
+    // takes in number of votes as prop
+    constructor(props) {
+
+    }
+
+    // updates vote counter when clicked
+    componentDidUpdate() {
+
+    }
+
+    // renders downvotebutton with proper vote count
+    render() {
+
+    }
+}
