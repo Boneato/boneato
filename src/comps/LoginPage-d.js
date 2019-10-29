e66086a9ccd5b46fba35448a536c720fa014f5db
Brian Uyeda <uyedacb@uw.edu>
2019-10-29 14:07:34 -0700

    create loginPage

--- src/comps/LoginPage-d.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react';
-import LoginController from '../../cont/LoginController';
-
-export class LoginPage extends Component {
-
-}
---
+++ src/comps/LoginPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import LoginController from '../../cont/LoginController';
+
+/
+export class LoginPage extends Component {
+    // takes in boolean variable indicating if user is logged in
+    constructor(props) {
+
+    }
+
+    // renders login page for sign in
+    // if user is signed in, redirects user to homePage2
+    render() {
+
+    }
+}
