d40f5bbbee886826f9494c0bb00428138b0ae7a9
Brian Uyeda <uyedacb@uw.edu>
2019-10-29 14:05:35 -0700

    update controller stubs

---
+++ src/cont/NewIngredientsController.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase';
+
+// takes in ingredient name and sends to database
+// table consisting of ingredients to be reviewd by admin
+export function sendIngred(ingredName) {
+
+}
---
+++ src/cont/PlacesController.js
@@ -0,0 +1,7 @@
+import GooglePlacesSuggest from "react-google-places-suggest"
+
+// uses give string search and returns relevant locations
+// if locations are outside of Seattle area, return an empty list
+export function validateLocation(search) {
+
+}
