325fac9a9d34a1c0724e78a01ecde6388cde4b52
Lauren Smith <leftcoastsmiths@gmail.com>
2019-11-12 10:53:06 -0800

    Card I think is done?

--- src/comps/modules/TeamCard.js
+++ src/comps/modules/TeamCard.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import Modal from '../modules/Modal';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 
 export class TeamCard extends Component {
     
@@ -15,5 +18,24 @@ export class TeamCard extends Component {
     // renders a team member image, team member first name, and team member role
     render() {
 
+        return (
+            <Card className={classes.card}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="/image_location.jpg"
+                  title="Person Photo"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Name Placeholder
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Position placeholder
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          );
     }
 }
