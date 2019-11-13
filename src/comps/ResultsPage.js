194f459bc0c1ae1e1bf8fd36a94d350618ad704d
Lauren Smith <leftcoastsmiths@gmail.com>
2019-11-12 16:25:58 -0800

    fixed list link

--- src/comps/ResultsPage.js
+++ src/comps/ResultsPage.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import {List, ListItem, ListItemLink, ListItemText} from '@material-ui/core';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+
+function ListItemLink(props) {
+    return <ListItem button component="a" {...props} />;
+  }
 
 export default class ResultsPage extends Component {
     // takes in list of links with attached ingredient IDs
     constructor(props) {
         
     }
+    
     
     // pre-conditions: 
     //      must be accessed directly from the HomePage
