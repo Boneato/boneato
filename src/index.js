267bb1f77a150da588d46e0eda05165e201ba03a
Brian Uyeda <uyedacb@uw.edu>
2019-11-12 16:49:36 -0800

    connect firestore

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import './css/index.css';
 import App from './comps/App';
 import * as serviceWorker from './serviceWorker';
 
+var config = {
+    apiKey: "AIzaSyA18aSKV9qp66x5s0FL6y_xcdjlBeBRVng",
+    authDomain: "bonito-collab.firebaseapp.com",
+    databaseURL: "https://bonito-collab.firebaseio.com",
+    projectId: "bonito-collab",
+    storageBucket: "bonito-collab.appspot.com",
+    messagingSenderId: "49830494344",
+    appId: "1:49830494344:web:f4a868b361ec362f571f3d",
+    measurementId: "G-683YLNRKFH"
+  };
+
+firebase.initializeApp(config);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
