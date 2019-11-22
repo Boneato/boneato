906a8f517b8ce85be6c3af3ad738818f40822566
xCyziw <neonlight003@gmail.com>
2019-11-21 16:39:46 -0800

    start homepage stuff

--- src/comps/HomePage.js
+++ src/comps/HomePage.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import SearchBarView from './modules/SearchBarView';
+import TextField from '@material-ui/core/TextField';
+
 
 export default class HomePage extends Component {
 	// pre-conditions: sign-in status recieved as true or false in props
@@ -12,6 +14,24 @@ export default class HomePage extends Component {
 	//      if user signed in, render "Signed in as 'username'" link
 	//      otherwise render "Sign In" link.
 	render() {
-		return(<body/>);
+		return (<div>
+
+			<div className="search-label">Where can I buy</div>
+
+			<TextField
+				id="outlined-search"
+				label="Search field"
+				type="search"
+				className="search-ingredient"
+				margin="normal"
+				variant="outlined"
+			/>
+
+
+			<div className="search-location">Seattle, WA</div>
+			<div className="search-description">Source hard-to-find ingredients for your next home-cooked meal.</div>
+
+
+		</div>);
 	}
 }
