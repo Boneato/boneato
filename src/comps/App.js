3ce882bb8335a74b8e4c5c73c90d573d616fea3e
anthonyqcn <anthonyqcn@gmail.com>
2019-11-30 14:25:44 -0800

    remove redundant results components

--- src/comps/App.js
+++ src/comps/App.js
@@ -1,63 +1,58 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import logo from '../assets/logo.svg';
 import '../css/App.css';
 import Navbar from './modules/Navbar.js';
 import HomePage from './HomePage';
 import LoginPage from './LoginPage';
-import ResultsPage from './ResultsPage';
 import SpecingPage from './SpecingPage';
 import AboutPage from './AboutPage';
-import {Route, Switch, Redirect} from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import LoginController from '../cont/LoginController';
-import {Modal, NewIngredientModal, NewLocationModal} from './modules/Modal';
+import { Modal, NewIngredientModal, NewLocationModal } from './modules/Modal';
 
 // renders application with all neccesary components
 export default class App extends Component {
+	// constructor(props) {
+	//   super(props);
+	//   this.state = {
+	//     user: '',
+	//   }
+	// }
 
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     user: '',
-  //   }
-  // }
+	componentDidMount() {
+		// this.fetchData();
+		// this.authUnSubFunction = firebase.auth().onAuthStateChanged((firebaseUser) => {
+		//   if (firebaseUser) {
+		//     this.setState(
+		//       {
+		//         user: firebaseUser
+		//       }
+		//     )
+		//   }
+		//   else {
+		//     this.setState({ user: null })
+		//   }
+		// })
+	}
 
-  componentDidMount() {
-    // this.fetchData();
-    // this.authUnSubFunction = firebase.auth().onAuthStateChanged((firebaseUser) => {
+	// if the user is signed-in, will log user out when exiting the web application
+	componentWillUnmount() {
+		//   this.authUnSubFunction()
+	}
 
-    //   if (firebaseUser) {
-    //     this.setState(
-    //       {
-    //         user: firebaseUser
-    //       }
-    //     )
-
-    //   }
-    //   else {
-    //     this.setState({ user: null })
-    //   }
-    // })
-  }
-
-  // if the user is signed-in, will log user out when exiting the web application
-  componentWillUnmount() {
- //   this.authUnSubFunction()
-  }
-
-  render() {
-    return (
-      <div>
-        <main>
-        <Navbar currentUser={LoginController.user}/>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/AboutPage" component={AboutPage} />
-            <Route path="/LoginPage" component={LoginPage} />
-            <Route path="/ResultsPage" component={ResultsPage} />
-            <Route path='/SpecIngPage' component={SpecingPage} />
-          </Switch>
-        </main>
-      </div>
-      );
-  }
+	render() {
+		return (
+			<div>
+				<main>
+					<Navbar currentUser={LoginController.user} />
+					<Switch>
+						<Route exact path="/" component={HomePage} />
+						<Route path="/AboutPage" component={AboutPage} />
+						<Route path="/LoginPage" component={LoginPage} />
+						<Route path="/SpecIngPage" component={SpecingPage} />
+					</Switch>
+				</main>
+			</div>
+		);
+	}
 }
