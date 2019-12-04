a6dae96c64cdb60104ee19764e1b0338007abd27
lyuyueCC <44285187+lyuyueCC@users.noreply.github.com>
2019-12-03 19:21:05 -0800

    newlocationmodal

--- src/cont/PlacesController.js
+++ src/cont/PlacesController.js
@@ -2,6 +2,23 @@
 
 // uses give string search and returns relevant locations
 // if locations are outside of Seattle area, return an empty list
-export function validateLocation(search) {
-
+export default function validateLocation(address) {
+    codeAddress(address);
 }
+
+function codeAddress(address) {
+    console.log("the codeAddress has been called!");
+    var address = address;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == 'OK') {
+        // map.setCenter(results[0].geometry.location);
+        // var marker = new google.maps.Marker({
+        //     map: map,
+        //     position: results[0].geometry.location
+        // });
+        console.log(results[0].geometry.location);
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
