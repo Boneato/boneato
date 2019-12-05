7606381010ef0e91adee9e33a5afb5d28faf1a1b
anthonyqcn <anthonyqcn@gmail.com>
2019-12-05 01:22:45 -0800

    Updates

--- src/cont/LocationsController.js
+++
@@ -1,8 +0,0 @@
-import firebase from 'firebase';
-require("firebase/firestore");
-
-// query database for list of LocationModel(s) for given ingredient ID.
-// return list of LocationModel(s).
-export function listLocations(ingredID) {
-
-}
--- src/cont/NewIngredientsController.js
+++
@@ -1,8 +0,0 @@
-import firebase from 'firebase/app';
-
-// takes in ingredient name and sends to database
-// table consisting of ingredients to be reviewd by admin
-export default function sendIngred(ingredName, ingredDescription) {
-    let ingredRef = firebase.database().ref('ingredients');
-    ingredRef.push({name: ingredName, descript: ingredDescription})
-}
